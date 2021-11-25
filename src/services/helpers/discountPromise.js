2e1762561a7986e83888d65315eceeb64e4d4e7a
kordonskanastya <kordonskanastya@gmail.com>
2021-11-25 13:23:32 +0200

    discount Promise

--- src/services/helpers/discountPromise.js
+++ src/services/helpers/discountPromise.js
@@ -8,35 +8,40 @@ function createDiscountPromise() {
         console.log(err);
         return reject(err);
       }
-      console.log(disc);
       return resolve(disc);
     });
   });
 
 }
 
+function discont() {
+  createDiscountPromise()
+.then((disc) => {
+  console.log(disc, 'first try');
+  return disc;
+})
+.catch(() => {
+  createDiscountPromise()
+  .then((disc) => {
+    console.log(disc, 'second try');
+    return disc;
+  })
+  .catch((err) => err);
+});
+}
+
 function addDiscountKey(obj) {
   const pricePerQuantity = obj.pricePerKilo || obj.pricePerItem;
   const weightOfFruit = obj.weight || obj.quantity;
-  const discont = Promise.all(createDiscountPromise())
-  .then((disc) => {
-    console.log(disc);
-    return disc;
-  })
-  .catch(() => {
-    createDiscountPromise()
-    .then((disc) => {
-      console.log(disc);
-      return disc;
-    })
-    .catch((err) => err);
-  });
   const discountPrice = formatPriceToNumber(pricePerQuantity)
     *
     weightOfFruit
     *
-    (100 - discont) / 100;
+    (100 - discont()) / 100;
   return { ...obj, discountPrice};
-};
+  };
+
+
+
 
 module.exports = (goodsArray) => goodsArray.map(addDiscountKey);
